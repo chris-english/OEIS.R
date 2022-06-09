4b50f40761d009b9f8ca4d7e5bb127a0de25805d
Chris English <englishchristophera@gmail.com>
2022-06-09 10:46:33 -0400

    Update OEIS_bfile.R
    _ not . in read_delim

--- R/OEIS_bfile.R
+++ R/OEIS_bfile.R
@@ -58,7 +58,7 @@ OEIS_bfile.OEIS_ID <- function(ID) {
     char0toNULL
   data <- lines[!match_comments]
 
-  data <- readr::read.delim(text = data,
+  data <- readr::read_delim(text = data,
                             colClasses = c("integer", "character"),
                             stringsAsFactors = FALSE)
   names(data) <- c("n", ID)
